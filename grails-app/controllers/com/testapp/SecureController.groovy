82681acffb03ecfef23e527f86cc18faa41efb72
Orlando Becerra <orlandobcrra@gmail.com>
2013-04-05 10:56:10 -0430

    add user/rol, secure a action

--- grails-app/controllers/com/testapp/SecureController.groovy
+++ grails-app/controllers/com/testapp/SecureController.groovy
@@ -1,8 +1,15 @@
 package com.testapp
 
+import grails.plugins.springsecurity.Secured
+
 class SecureController {
 
+	@Secured(['ROLE_ADMIN'])
     def index() {
     	render 'Secure access only'
+    }
+
+    def normal() {
+    	render 'normal access'
     }
 }
